c0612bd21b398903d06210e8b931a33bb11ccc03
Daniel C <daniel-2.010@hotmail.com>
2019-09-02 20:00:50 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+    <GlobalStyle />
+    <Header />
+    <Board />
+    
+    </DndProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,58 @@
+import React, { useRef } from 'react';
+
+import { useDrag, useDrop } from 'react-dnd';
+
+import { Container, Label } from './styles';
+
+export default function Card({data, index}){
+    const ref = useRef();
+
+    const [{isDragging}, dragRef] = useDrag({
+        item: { type: 'CARD', index},
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        })
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor){
+            const draggedIndex = item.idex;
+            const targetIndex = index;
+
+            if(draggedIndex === targetIndex){
+                return;
+            }
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = (targetSize.bottom - targetSize.top) /2;
+
+            const draggedOffset = monitor.getClientOffset();
+
+            const draggedTop = draggedOffset.y - targetSize.top;
+
+            if(draggedIndex < targetIndex && draggedTop < targetCenter){
+                return;
+            }
+
+            if(draggedIndex > targetIndex && draggedTop > targetCenter){
+                return;
+            }
+
+
+        }
+    });
+
+    dragRef(dropRef(ref));
+
+    return(
+        <Container ref={ref} isDragging={isDragging}>
+            <header>
+                {data.labels.map(label => <Label key={label} color={label} />)}
+            </header>
+            <p>
+                {data.content}
+            </p>
+            { data.user && <img src={data.user} alt="" />}
+        </Container>
+    )
+}
